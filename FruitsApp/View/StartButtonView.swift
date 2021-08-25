35f23e9f1ebed492aab9c5aa4dc843c45634a223
Antonio Massa <antonio.massa@zehus.it>
2021-08-25 10:39:27 +0200

    Created 2 new View files.
    FruitCardView : Made a main view for the fruit, with background, images, and text.
    StartButtonView: Code coming next

---
+++ FruitsApp/View/StartButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  StartButtonView.swift
+//  StartButtonView
+//
+//  Created by Massa Antonio on 25/08/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}

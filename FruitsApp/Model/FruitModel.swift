6358e10363ac435dae75c3ff37f3599aab01b377
Antonio Massa <antonio.massa@zehus.it>
2021-08-25 12:20:14 +0200

    - Added a new Group called Model: Created a FrutiModel SwiftFile with a struct Fruit: Identifiable - Added a new Group called Data: Created a new Swiftfile called FruitData, passed in all the Text from the Resources folder - Changed in FruitCardView all the text with the FruitData proprieties - In OnboardingView created a proprieties called fruit, passing it Fruit struct created in FruitModel; Modified the ForEach loop with existing fruits. In previews called fruitsData

---
+++ FruitsApp/Model/FruitModel.swift
@@ -0,0 +1,18 @@
+//
+//  FruitModel.swift
+//  FruitModel
+//
+//  Created by Massa Antonio on 25/08/21.
+//
+
+import SwiftUI
+
+struct Fruit: Identifiable {
+	var id = UUID()
+	var title: String
+	var headline: String
+	var image: String
+	var gradientColors: [Color]
+	var description: String
+	var nutrition: [String]
+}
